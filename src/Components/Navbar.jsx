5dfbe4a626571803c7ed7705e20e215ca34b4260
dheeraj prajapati <dhirajprajapati596@gmail.com>
2024-09-05 15:21:26 +0530

    Pexels-Verse

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,25 +1,85 @@
-import React, { useContext } from 'react'
-import PixabayContext from '../Context/PixabayContext'
+
+
+import React, { useContext } from 'react';
+import PixabayContext from '../Context/PixabayContext';
 
 const Navbar = () => {
- const {fetchImageBycategory,setQuery } = useContext(PixabayContext)
+  const { fetchImageBycategory, setQuery } = useContext(PixabayContext);
+
   return (
     <>
-    <div className='container text-center my-3'> 
-     <button type="button"onClick={()=>fetchImageBycategory("nature")} className="btn btn-outline-primary mx-3">Nature</button>
-<button type="button"onClick={()=>fetchImageBycategory("science")} className="btn btn-outline-warning mx-3">Science</button>
-<button type="button"onClick={()=>fetchImageBycategory("computer")} className="btn btn-outline-success mx-3">Computer</button>
-<button type="button"onClick={()=>fetchImageBycategory("business")} class="btn btn-outline-danger mx-3">Business</button>
-<button type="button"onClick={()=>fetchImageBycategory("buildings")} className="btn btn-outline-warning mx-3">Buildings</button>
-<button type="button"onClick={()=>fetchImageBycategory("sports")} className="btn btn-outline-info mx-3">Sports</button>
-<button type="button"onClick={()=>fetchImageBycategory("transportation")} className="btn btn-outline-light mx-3">Transportation</button>
-<button type="button"onClick={()=>fetchImageBycategory("food")} className="btn btn-outline-dark mx-3">Food</button>
-    </div>
-    <div className='container' style={{width:"800px"}}>
-      <input type="text" onChange={(e)=>setQuery(e.target.value)} className="form-control bg-dark  text-light"></input>
-    </div>
+      <div className='container text-center my-3'>
+        <div className='row'>
+          <div className='col-12'>
+            <button
+              type="button"
+              onClick={() => fetchImageBycategory("nature")}
+              className="btn btn-outline-primary mx-2 mb-2 mb-md-0"
+            >
+              Nature
+            </button>
+            <button
+              type="button"
+              onClick={() => fetchImageBycategory("science")}
+              className="btn btn-outline-warning mx-2 mb-2 mb-md-0"
+            >
+              Science
+            </button>
+            <button
+              type="button"
+              onClick={() => fetchImageBycategory("computer")}
+              className="btn btn-outline-success mx-2 mb-2 mb-md-0"
+            >
+              Computer
+            </button>
+            <button
+              type="button"
+              onClick={() => fetchImageBycategory("business")}
+              className="btn btn-outline-danger mx-2 mb-2 mb-md-0"
+            >
+              Business
+            </button>
+            <button
+              type="button"
+              onClick={() => fetchImageBycategory("buildings")}
+              className="btn btn-outline-warning mx-2 mb-2 mb-md-0"
+            >
+              Buildings
+            </button>
+            <button
+              type="button"
+              onClick={() => fetchImageBycategory("sports")}
+              className="btn btn-outline-info mx-2 mb-2 mb-md-0"
+            >
+              Sports
+            </button>
+            <button
+              type="button"
+              onClick={() => fetchImageBycategory("transportation")}
+              className="btn btn-outline-light mx-2 mb-2 mb-md-0"
+            >
+              Transportation
+            </button>
+            <button
+              type="button"
+              onClick={() => fetchImageBycategory("food")}
+              className="btn btn-outline-dark mx-2 mb-2 mb-md-0"
+            >
+              Food
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className='container' style={{ maxWidth: "100%" }}>
+        <input
+          type="text"
+          onChange={(e) => setQuery(e.target.value)}
+          className="form-control bg-dark text-light"
+          placeholder="Search..."
+        />
+      </div>
     </>
-  )
+  );
 }
- 
-export default Navbar
+
+export default Navbar;
